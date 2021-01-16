857ab76969b60a5805aa4d60310ad53353d7b49a
Eduard Mandy <mandyedi@gmail.com>
2021-01-17 00:14:10 +0100

    [#11] Add copyright notice to every source file

--- src/Cameras/Camera.cpp
+++ src/Cameras/Camera.cpp
@@ -1,3 +1,14 @@
+//  Copyright (C) Kevin Suffern 2000-2007.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+//  Copyright notices for changes since the originally published version:
+//  Copyright (C) Eduárd Mándy 2019-2021
+//  Though this C++ code was change in a large measure it still has the original copyright notices.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
 
 #include "Camera.h"
 
