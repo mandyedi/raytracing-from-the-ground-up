f56eabc80e726f2b964d8af1ddbb2d17f5aa606e
mandyedi <mandyedi@gmail.com>
2021-01-24 21:41:58 +0000

    [#5 10 Depth of Field] Add ThinLens class

---
+++ src/Cameras/ThinLens.h
@@ -0,0 +1,54 @@
+//  Copyright (C) Kevin Suffern 2000-2007.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+//  Copyright notice for changes since the originally published version:
+//  Copyright (C) Eduárd Mándy 2019-2021
+//  Though this C++ code was change in a large measure it still has the original copyright notice.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+#ifndef __THINLENS__
+#define __THINLENS__
+
+#include "../Utilities/Point2D.h"
+#include "../World/World.h"
+
+class ThinLens: public Camera {
+
+	public:
+
+		ThinLens();
+
+		ThinLens(const ThinLens& ph);
+
+		virtual Camera*
+		clone(void) const override;
+
+		ThinLens&
+		operator= (const ThinLens& rhs);
+
+		virtual
+		~ThinLens();
+
+		void
+		set_sampler(Sampler* sp);
+
+		Vector3D
+		ray_direction(const Point2D& pixel_point, const Point2D& lens_point) const;
+
+		virtual void
+		render_scene(const World& w) override;
+
+	private:
+
+		float		lens_radius;	// lens radius
+		float		d;				// view plane distance
+		float		f;				// focal distance
+		float		zoom;			// zoom factor
+		Sampler*	sampler_ptr;	// sampler object
+};
+
+#endif
