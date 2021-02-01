f36569774db89e72438d66c9b70321cf2a79fc40
Eduard Mandy <mandyedi@gmail.com>
2021-02-02 00:36:15 +0100

    Fix errors

--- src/Textures/Checker3D.cpp
+++ src/Textures/Checker3D.cpp
@@ -1,10 +1,11 @@
 #include "Checker3D.h"
 #include "../Utilities/Constants.h"
+#include "../Utilities/RGBColor.h"
 
 Checker3D::Checker3D(void)
     : Texture(),
       size(1.0),
-      color1(white),
+      color1(RGBColor::white),
       color2(0.5)
 {}
 
@@ -39,10 +40,10 @@ Checker3D::~Checker3D(void) {}
 
 RGBColor
 Checker3D::get_color(const ShadeRec& sr) const {
-    float eps = -0.000187453738;	// small random number
-    float x = sr.local_hit_point.x + eps;
-    float y = sr.local_hit_point.y + eps;
-    float z = sr.local_hit_point.z + eps;
+    double eps = -0.000187453738;	// small random number
+    double x = sr.local_hit_point.x + eps;
+    double y = sr.local_hit_point.y + eps;
+    double z = sr.local_hit_point.z + eps;
 
     if (((int)floor(x / size) + (int)floor(y / size) + (int)floor(z / size)) % 2 == 0) {
         return (color1);
