050fe6084bc11e170722c06a5ded6f4dddba7268
Eduard Mandy <mandyedi@gmail.com>
2022-03-27 22:28:22 +0200

    [#5 19.3 Triangles]

---
+++ src/GeometricObjects/Triangle.cpp
@@ -0,0 +1,189 @@
+//  Copyright (C) Kevin Suffern 2000-2007.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+//  Copyright notice for changes since the originally published version:
+//  Copyright (C) Eduárd Mándy 2019-2021
+//  Though this C++ code was change in a large measure it still has the original copyright notice.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+#include <utility>
+#include <limits>
+#include "Triangle.h"
+
+
+
+Triangle::Triangle (const Point3D& a, const Point3D& b, const Point3D& c)
+	: 	GeometricObject(),
+		v0(a),
+		v1(b),
+		v2(c)
+{
+	compute_normal();
+}
+
+
+
+Triangle::Triangle (const Triangle& triangle)
+	:	GeometricObject(triangle),
+		v0(triangle.v0),
+		v1(triangle.v1),
+		v2(triangle.v2),
+		normal(triangle.normal)
+{}
+
+
+
+Triangle::Triangle (Triangle&& triangle) noexcept
+	:	GeometricObject(std::move(triangle)),
+		v0(std::move(triangle.v0)),
+		v1(std::move(triangle.v1)),
+		v2(std::move(triangle.v2)),
+		normal(std::move(triangle.normal))
+{}
+
+
+
+Triangle&
+Triangle::operator= (const Triangle& triangle) {
+	GeometricObject::operator=(triangle);
+
+	v0 		= triangle.v0;
+	v1 		= triangle.v1;
+	v2 		= triangle.v2;
+	normal 	= triangle.normal;
+
+	return (*this);
+}
+
+
+
+Triangle&
+Triangle::operator= (Triangle&& triangle) noexcept {
+	GeometricObject::operator=(std::move(triangle));
+
+	v0 		= std::move(triangle.v0);
+	v1 		= std::move(triangle.v1);
+	v2 		= std::move(triangle.v2);
+	normal 	= std::move(triangle.normal);
+
+	return (*this);
+}
+
+
+
+Triangle*
+Triangle::clone(void) const {
+	return (new Triangle(*this));
+}
+
+
+
+void
+Triangle::compute_normal(void) {
+	normal = (v1 - v0) ^ (v2 - v0);
+	normal.normalize();
+}
+
+
+BBox
+Triangle::get_bounding_box(void) {
+	double delta = 0.000001;
+
+	return (BBox(std::min(std::min(v0.x, v1.x), v2.x) - delta, std::max(std::max(v0.x, v1.x), v2.x) + delta,
+				 std::min(std::min(v0.y, v1.y), v2.y) - delta, std::max(std::max(v0.y, v1.y), v2.y) + delta,
+				 std::min(std::min(v0.z, v1.z), v2.z) - delta, std::max(std::max(v0.z, v1.z), v2.z) + delta));
+}
+
+
+
+bool
+Triangle::hit(const Ray& ray, double& tmin, ShadeRec& sr) const {
+	double a = v0.x - v1.x, b = v0.x - v2.x, c = ray.d.x, d = v0.x - ray.o.x;
+	double e = v0.y - v1.y, f = v0.y - v2.y, g = ray.d.y, h = v0.y - ray.o.y;
+	double i = v0.z - v1.z, j = v0.z - v2.z, k = ray.d.z, l = v0.z - ray.o.z;
+
+	double m = f * k - g * j, n = h * k - g * l, p = f * l - h * j;
+	double q = g * i - e * k, s = e * j - f * i;
+
+	double inv_denom  = 1.0 / (a * m + b * q + c * s);
+
+	double e1 = d * m - b * n - c * p;
+	double beta = e1 * inv_denom;
+
+	if (beta < 0.0) {
+		return (false);
+	}
+
+	double r = r = e * l - h * i;
+	double e2 = a * n + d * q + c * r;
+	double gamma = e2 * inv_denom;
+
+	if (gamma < 0.0) {
+		return (false);
+	}
+
+	if (beta + gamma > 1.0) {
+		return (false);
+	}
+
+	double e3 = a * p - b * r + d * s;
+	double t = e3 * inv_denom;
+
+	if (t < std::numeric_limits<double>::epsilon()) {
+		return (false);
+	}
+
+	tmin 				= t;
+	sr.normal 			= normal;
+	sr.local_hit_point 	= ray.o + t * ray.d;
+
+	return (true);
+}
+
+
+
+bool
+Triangle::shadow_hit(const Ray& ray, double& tmin) const {
+	double a = v0.x - v1.x, b = v0.x - v2.x, c = ray.d.x, d = v0.x - ray.o.x;
+	double e = v0.y - v1.y, f = v0.y - v2.y, g = ray.d.y, h = v0.y - ray.o.y;
+	double i = v0.z - v1.z, j = v0.z - v2.z, k = ray.d.z, l = v0.z - ray.o.z;
+
+	double m = f * k - g * j, n = h * k - g * l, p = f * l - h * j;
+	double q = g * i - e * k, s = e * j - f * i;
+
+	double inv_denom  = 1.0 / (a * m + b * q + c * s);
+
+	double e1 = d * m - b * n - c * p;
+	double beta = e1 * inv_denom;
+
+	if (beta < 0.0) {
+		return (false);
+	}
+
+	double r = e * l - h * i;
+	double e2 = a * n + d * q + c * r;
+	double gamma = e2 * inv_denom;
+
+	if (gamma < 0.0) {
+		return (false);
+	}
+
+	if (beta + gamma > 1.0) {
+		return (false);
+	}
+
+	double e3 = a * p - b * r + d * s;
+	double t = e3 * inv_denom;
+
+	if (t < std::numeric_limits<double>::epsilon()) {
+		return (false);
+	}
+
+	tmin = t;
+
+	return(true);
+}
---
+++ src/GeometricObjects/Triangle.h
@@ -0,0 +1,62 @@
+//  Copyright (C) Kevin Suffern 2000-2007.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+//  Copyright notice for changes since the originally published version:
+//  Copyright (C) Eduárd Mándy 2019-2021
+//  Though this C++ code was change in a large measure it still has the original copyright notice.
+//  This C++ code is for non-commercial purposes only.
+//  This C++ code is licensed under the GNU General Public License Version 2.
+//  See the file COPYING.txt for the full license.
+
+#ifndef __TRIANGLE__
+#define __TRIANGLE__
+
+
+
+#include "GeometricObject.h"
+
+class Triangle: public GeometricObject {
+	public:
+
+		Triangle(void) = default;
+
+		explicit Triangle(const Point3D& a, const Point3D& b, const Point3D& c);
+
+		~Triangle(void) = default;
+
+		Triangle(const Triangle& triangle);
+
+		Triangle(Triangle&& triangle) noexcept;
+
+		Triangle&
+		operator= (const Triangle& triangle);
+
+		Triangle&
+		operator= (Triangle&& triangle) noexcept;
+
+		virtual Triangle*
+		clone(void) const;
+
+		virtual BBox
+		get_bounding_box(void);
+
+		void
+		compute_normal(void);
+
+		virtual bool
+		hit(const Ray& ray, double& tmin, ShadeRec& sr) const;
+
+		virtual bool
+		shadow_hit(const Ray& ray, double& tmin) const;
+
+	private:
+
+		Point3D	v0 = Point3D(0.0, 0.0, 0.0);
+		Point3D	v1 = Point3D(0.0, 0.0, 1.0);
+		Point3D	v2 = Point3D(1.0, 0.0, 0.0);
+		Normal	normal = Normal(0.0, 1.0, 0.0);
+};
+
+#endif
